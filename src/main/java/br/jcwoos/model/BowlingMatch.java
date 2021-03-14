8b9a068a5cca62726caf9425382b50bc486968b9
Julio Cesar Woos <julio.woos@datapar.com.py>
2021-03-14 16:43:20 -0300

    using LinkedHashMap to keep the order of the players

--- src/main/java/br/jcwoos/model/BowlingMatch.java
+++ src/main/java/br/jcwoos/model/BowlingMatch.java
@@ -1,6 +1,7 @@
 package br.jcwoos.model;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -27,7 +28,7 @@ public class BowlingMatch {
 
 		public Builder(List<Roll> matchRolls) throws WrongNumberOfRollsException, InvalidScoreException {
 			results = new ArrayList<>();
-			Map<String, List<Roll>> groupedRolls = matchRolls.stream().collect(Collectors.groupingBy(Roll::getPlayer));
+			Map<String, List<Roll>> groupedRolls = matchRolls.stream().collect(Collectors.groupingBy(Roll::getPlayer, LinkedHashMap::new, Collectors.toList()));
 
 			for (Entry<String, List<Roll>> entry : groupedRolls.entrySet()) {
 				results.add(new PlayerResult.Builder(entry.getKey(), entry.getValue()).build());
--- src/test/java/br/jcwoos/BowlingMatchResultsPrinterStandardOutTest.java
+++ src/test/java/br/jcwoos/BowlingMatchResultsPrinterStandardOutTest.java
@@ -73,12 +73,12 @@ public class BowlingMatchResultsPrinterStandardOutTest {
 	public void shoulHaveThisOutputForTheSample() throws BowlingException, UnsupportedEncodingException {
 		RollParser rollParser = new DefaultRollParser(new FileReaderNio("sample_cases/sample1.txt"));
 		String expected = "Frame\t\t1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8\t\t9\t\t10\n" +
-		        "John\n" +
-		        "Pinfalls\t3\t/\t6\t3\t\tX\t8\t1\t\tX\t\tX\t9\t0\t7\t/\t4\t4\tX\t9\t0\n" +
-		        "Score\t\t16\t\t25\t\t44\t\t53\t\t82\t\t101\t\t110\t\t124\t\t132\t\t151\n" +
 		        "Jeff\n" +
 		        "Pinfalls\t\tX\t7\t/\t9\t0\t\tX\t0\t8\t8\t/\tF\t6\t\tX\t\tX\tX\t8\t1\n" +
-		        "Score\t\t20\t\t39\t\t48\t\t66\t\t74\t\t84\t\t90\t\t120\t\t148\t\t167\n";
+		        "Score\t\t20\t\t39\t\t48\t\t66\t\t74\t\t84\t\t90\t\t120\t\t148\t\t167\n" +
+		        "John\n" +
+		        "Pinfalls\t3\t/\t6\t3\t\tX\t8\t1\t\tX\t\tX\t9\t0\t7\t/\t4\t4\tX\t9\t0\n" +
+		        "Score\t\t16\t\t25\t\t44\t\t53\t\t82\t\t101\t\t110\t\t124\t\t132\t\t151\n";
 
 		BowlingMatch match = new BowlingMatch.Builder(rollParser.parseLines()).build();
 
